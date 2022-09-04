20dcb966905b96ce11089df22ad3bf33f688c628
unknown <asifnoushad61@gmail.com>
2022-09-04 10:05:08 +0530

    Updates

---
+++ precache-manifest.a3ce6a7370e251da7f1099632174b6e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f1ff0495bf828a39a926",
+    "url": "/Disney_Clone/static/css/main.75b630ca.chunk.css"
+  },
+  {
+    "revision": "f1ff0495bf828a39a926",
+    "url": "/Disney_Clone/static/js/main.f1ff0495.chunk.js"
+  },
+  {
+    "revision": "899dc40d2a92699b4693",
+    "url": "/Disney_Clone/static/js/1.899dc40d.chunk.js"
+  },
+  {
+    "revision": "e10d5c980d2cfe78c25f",
+    "url": "/Disney_Clone/static/js/runtime~main.e10d5c98.js"
+  },
+  {
+    "revision": "22cb55ed2ab481ff96ffd352592501ac",
+    "url": "/Disney_Clone/index.html"
+  }
+];
